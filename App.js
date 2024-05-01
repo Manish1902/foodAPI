4f909de6506d5a062db779fea4c35831993bb8a4
Manish Singh Talan <manishtalan19@gmail.com>
2024-05-01 23:29:41 +0530

    food aap

---
+++ App.js
@@ -0,0 +1,16 @@
+import { createStackNavigator} from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import SearchScreen from "./src/Screens/SearchScreen";
+import ResultShowScreen from "./src/Screens/ResultShowScreen";
+
+const navigator = createStackNavigator({
+  Search: SearchScreen,
+  ResultShow: ResultShowScreen,
+},{
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
+})
+
+export default createAppContainer(navigator);
